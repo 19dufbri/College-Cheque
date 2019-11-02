cd41d49bf9381f6998e1dd483b70e6946290cc4e
Brian Duffy <bgd1112@gmail.com>
2019-11-02 16:32:31 -0400

    Blah

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
@@ -8,6 +9,12 @@ def index():
 @app.route('/results')
 def results():
     return render_template("results.html", numResults=5)
+
+def initalize():
+	with open('file.csv') as csvDataFile:
+    	csvReader = csv.reader(csvDataFile)
+    	for row in csvReader:
+        	print(row)
 
 if __name__ == '__main__':
     app.run(debug=False)
