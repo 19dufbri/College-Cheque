bc6c3c8bd91c7da1ac8f2a595d5e897987461941
alanqchen <alanchen1908@gmail.com>
2019-11-03 09:15:54 -0500

    Fix pie-chart labels

--- app/app.py
+++ app/app.py
@@ -362,6 +362,7 @@ def processRow(row):
     return row
 
 def makePie(dict):
+    plt.figure()
     labels = []
     percents = []
     colors = ["#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9"]
